7b1d4418940f224bcada72112657f68b1db4556a
mac <mac@macs-MacBook-Pro.local>
2024-05-08 20:07:36 +0700

    add ddl

--- ddl.sql
+++ ddl.sql
@@ -1,12 +1,12 @@
 CREATE TABLE poi (
-    id INT(11) PRIMARY KEY AUTO_INCREMENT,
-    latitude DECIMAL(10,8) NULL,
-    longitude DECIMAL(11,8) NULL,
-    nama VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
-    description TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
-    category VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
-    rating DECIMAL(3,2) NULL,
-    contact VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    latitude DECIMAL(10, 8),
+    longitude DECIMAL(11, 8),
+    nama VARCHAR(255),
+    description TEXT,
+    category VARCHAR(50),
+    rating DECIMAL(3, 2),
+    contact VARCHAR(255)
 );
 
 
